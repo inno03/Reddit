94ebdfe55b3d8fced160f6afc3e64f1bfe2388dd
Innocent <rupiahinnocent66@gmail.com>
2022-09-21 21:12:14 +0300

    Commit

---
+++ components/Comments.js
@@ -0,0 +1,46 @@
+import timeago from 'lib/timeago'
+import { useState } from 'react'
+import NewComment from 'components/NewComment'
+
+const Comment = ({ comment, post }) => {
+  const [showReply, setShowReply] = useState(false)
+  return (
+    <div className=' mt-6'>
+      <p>
+        {comment.author.name} {timeago.format(new Date(comment.createdAt))}
+      </p>
+      <p>{comment.content}</p>
+      {showReply ? (
+        <div className='pl-10'>
+          <NewComment comment={comment} post={post} />
+        </div>
+      ) : (
+        <p
+          className='underline text-sm cursor-pointer'
+          onClick={() => setShowReply(true)}
+        >
+          reply
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default function Comments({ comments, post }) {
+  if (!comments) return null
+
+  return (
+    <>
+      {comments.map((comment, index) => (
+        <div key={index}>
+          <Comment key={index} comment={comment} post={post} />
+          {comment.comments && (
+            <div className='pl-10'>
+              <Comments comments={comment.comments} post={post} />
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  )
+}
